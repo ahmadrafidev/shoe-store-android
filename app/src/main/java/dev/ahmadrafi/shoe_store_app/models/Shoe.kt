84e0a0404e307a2d3f4732658cfbf0c68374f5c6
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-12 17:42:56 +0700

    feat: adding navigation plugin and initiating shoe data class

---
+++ app/src/main/java/dev/ahmadrafi/shoe_store_app/models/Shoe.kt
@@ -0,0 +1,14 @@
+package dev.ahmadrafi.shoe_store_app.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Shoe(
+    var name: String,
+    var size: Double,
+    var company: String,
+    var description: String,
+
+    val images: List<String> = mutableListOf()
+) : Parcelable
